3a58ea4539147f37f63ffd3167676388dfff0555
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-10-26 15:57:49 +0300

    deleteing temp file

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-rows = int(input("Enter the number of rows: "))
-
-for i in range(rows):
-  print(" " * (rows - i) + "*" * (2 * i - 1))
-for i in range(rows - 2, 0, -1):
-  print(" " * (rows - i) + "*" * (2 * i - 1))
